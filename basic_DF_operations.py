cd5344fdaca4cef8db5964cb029c7aab322f023f
Mike <docteur.zuf@gmail.com>
2018-04-05 17:29:40 +0200

    Update basic_DF_operations.py

--- basic_DF_operations.py
+++ basic_DF_operations.py
@@ -31,13 +31,32 @@ a.withColumn("Result", F.mean(a['A']).over(window_a)).show()
 a.groupby('Phase').agg({'FF_1': 'avg'}).show()
 
 def phase_detection(df):
+    '''
+    Detects when the value of column 'Phase' changes and attribute
+    its value to a new column
+    '''
     det_phase = (F.lag(col('Phase'), 0).over(window_a)) !=\
-                    (F.lead(col('Phase'), 1).over(window_a))
-    #det_end = (F.last(col('Phase')).over(window_b)).isNull() 
+                    (F.lead(col('Phase'), 1).over(window_a))    
     det_end = (F.lead(col('Phase'), 1).over(window_a)).isNull()    
     result_phase = (when(det_phase, df['Phase'])\
                    .when(det_end, df['Phase'])\
                    .otherwise(0))
     return result_phase
+  
+# Sum of a column
+a.select(F.sum(a['FF_1'])).show()
+# Mean
+a.select(F.mean(a['FF_1'])).show()
+a.select(F.avg(a['FF_1'])).show()
 
+# Cumulative sum
+windowSpec = Window.orderBy('ID').rowsBetween(Window.unboundedPreceding, Window.currentRow)
+a.withColumn('nb_flights', F.sum(F.col('FF_1')).over(windowSpec)).show()
 sum_cols = udf(lambda x: x[0]+x[1], IntegerType())
+
+''' 
+To collect a value from a DataFrame
+'''
+b = a.select(F.sum(a['FF_1']))
+# We have to collect() and select first element since it's a list
+c = b.collect()[0]
